89b191f1e0c28ae62865a0e8ea605c10c9d3082d
Aejas Naikcd <aejasnaik@hotmail.com>
2026-02-15 19:57:42 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import WTOChart from './WTO_DualLine_Graph_Fixed';
+
+function App() {
+  return (
+    <div>
+      <h1>WTO Dual Line Chart</h1>
+      <WTOChart />
+    </div>
+  );
+}
+
+export default App;
